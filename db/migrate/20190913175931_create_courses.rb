f3299c88eb0e3dd7af1a5d8e80113fd8552c60a8
Timgriffin1 <timgriffin1@gmail.com>
2019-09-17 18:49:44 +0000

    student courses

--- db/migrate/20190913175931_create_courses.rb
+++ db/migrate/20190913175931_create_courses.rb
@@ -2,9 +2,7 @@ class CreateCourses < ActiveRecord::Migration[5.2]
   def change
     create_table :courses do |t|
       t.string :title
-      t.text :description
-      t.decimal :cost
-      t.integer :user_id
+      t.integer :course_id
       t.timestamps
     end
     add_index :courses, :user_id
