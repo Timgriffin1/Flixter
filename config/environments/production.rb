fda725d8a300bb08bfc762c8a41ed393ad943c3e
Timgriffin1 <timgriffin1@gmail.com>
2019-11-06 16:13:14 +0000

    heroku flixter push

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  flixter-tim-griffin.herokuapp.com
+ 
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
